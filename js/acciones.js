1574847b59626963d6d8e29298f3a1b43150dd01
AL41F13 <janeth982210@gmail.com>
2015-12-07 12:42:29 -0600

    nueve
    diez

--- js/acciones.js
+++ js/acciones.js
@@ -31,6 +31,36 @@ $('#btnlistado').on('tap',function(){
 		}
 			});
 			});
+			// aqui inicia la parte de la busqueda por el nombre
+			$('#busca_d').on('tap',function(){
+	
+	$.ajax({
+		
+		type:"POST",
+		
+		url:"http://192.168.1.188/dulce/busqueda_nombre.php",
+		
+		data: "nombre=" + $('#txtnombre').val(),
+		
+		error: function(){alert("error en conexion");},
+		
+		success: function(respuesta){var producto = JSON.parse(respuesta);
+		alert(respuesta);
+		
+		$('#contenido').empty();
+		
+		for(var $x=0; $x<producto.dulce.length;$x++)
+		
+		{$('#contenido').append('   <div><div align="center" style="border:4px solid #63F;"><div>'+ producto.dulce[$x].nombreDulce +' </div><div>'+ producto.dulce[$x].regionDulce +'  </div><div>                '+ producto.dulce[$x].descripcionDulce +'</div><div><img src="http://192.168.1.188/dulce/recursos/imagenes/fotos/'+ producto.dulce[$x].claveDulce +'.jpg"> </div></div></div></div>      ');}
+		
+		
+	
+		
+		
+			
+		}
+			});
+			});
 }); 
 });
 
